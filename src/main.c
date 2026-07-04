ef61d2efd4f6c9a1a2d78fa3ba0f2c570c22d879
Utkarsh Goyal <goyalutkarsh330@gmail.com>
2026-07-04 22:52:02 +0530

    Added octal and hexadecimal conversion

--- src/main.c
+++ src/main.c
@@ -3,7 +3,9 @@
 //  Write a C program that takes any number number and converts it to binary, octal, and hex — displaying it as actual bit patterns. Then extend it to perform AND, OR, XOR on two numbers and show the result bit by bit. This teaches you bit manipulation in C (which BEL interviews ask directly) and reinforces number systems from their digital electronics syllabus. Every time you write n & 1 or n >> 1 you're doing what BEL's technical paper tests — just in code form instead of theory.
 void print_binary_no(int );
 
+void print_octal_no(int);
 
+void print_hexa_no(int);
 
 int main() {
     int number;
@@ -12,14 +14,75 @@ int main() {
     scanf("%d",&number);
 
     print_binary_no(number);
-
+    print_octal_no(number);
+    print_hexa_no(number);
     return 0;
 }
 void print_binary_no(int n){
-    printf("The binary representation of %d is : ",n);
-    
-    for(int i=7 ; i>=0 ;i--){
-        printf("%d",(n>>i & 1));
+    printf("The binary representation of %d is: ",n);
+
+    int started = 0; /*started variable for eliminating the extra zeros from the 32 bit representation to make the code look good*/
+
+    for (int i = 31; i >= 0; i--)
+    {
+        int bit = (n >> i) & 1;
+
+        if (bit || started || i == 0)
+        {
+            printf("%d", bit);
+            started = 1;
+        }
     }
+
+    printf("\n");
+}
+
+
+
+void print_octal_no(int n)
+{
+    printf("Octal representation : ");
+
+    int started = 0;
+
+    // here we are extracting 3 bits at a time
+
+    for (int i = 30; i >= 0; i -= 3) 
+     //30 liya kyuki apan ko 32 bits ko 3-3 ke grps main divide krna hain oth   strting with (0,1,2) so on we get to (28,29,30)//aur aise krke apan ne 31 tk cover krli
+
+    {
+        int digit = (n >> i) & 7;     // 7 = 111
+
+        if (digit || started || i == 0)
+        {
+            printf("%d", digit);
+            started = 1;
+        }
+    }
+
+    printf("\n");
+}
+
+void print_hexa_no(int n){
+    printf("Hexadecimal representation: ");
+
+    int started = 0;
+
+    // Extract 4 bits at a time
+    for (int i = 28; i >= 0; i -= 4)
+    {
+        int digit = (n >> i) & 15;    // 15 = 1111
+
+        if (digit || started || i == 0)
+        {
+            if (digit < 10)
+                printf("%d", digit);
+            else
+                printf("%c", 'A' + digit - 10);
+
+            started = 1;
+        }
+    }
+
     printf("\n");
 }
